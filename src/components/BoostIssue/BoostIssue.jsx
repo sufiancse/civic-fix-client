7ea64ca4038725cb9ff97bba7c2381ced1267239
sufuancse <sufian.cse24@gmail.com>
2025-12-18 08:37:19 +0600

    home page and all issue page all requirement implement done

--- src/components/BoostIssue/BoostIssue.jsx
+++ src/components/BoostIssue/BoostIssue.jsx
@@ -2,35 +2,57 @@ import axios from "axios";
 import React from "react";
 import useAuth from "../../hooks/useAuth";
 import { FaBolt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const BoostIssue = ({ issue }) => {
-
-  const {user} = useAuth()
+  const { user } = useAuth();
 
   const handleBoost = async () => {
-    const paymentInfo = {
-      issueId: issue._id,
-      issueTitle: issue.title,
-      issueReportedBy: issue.issueBy,
-      issueBoostedBy: user?.email,
-      price: 100,
-    };
-
-    console.log(paymentInfo);
-
-    const { data } = await axios.post(
-      `${import.meta.env.VITE_API_URL}/create-issue-boost-checkout-session`,
-      paymentInfo
-    );
-    window.location.href = data?.url;
+  const paymentInfo = {
+    issueId: issue._id,
+    issueTitle: issue.title,
+    issueReportedBy: issue.issueBy,
+    issueBoostedBy: user?.email,
+    price: 100,
   };
+
+  Swal.fire({
+    title: "Boost Priority for 100 Tk",
+    text: "Confirm to boost this issue's priority. This action cannot be undone!",
+    icon: "info",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, Boost Now!",
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      try {
+        const { data } = await axios.post(
+          `${import.meta.env.VITE_API_URL}/create-issue-boost-checkout-session`,
+          paymentInfo
+        );
+        window.location.href = data?.url;
+      } catch (error) {
+        Swal.fire({
+          title: "Payment Failed",
+          text: "Something went wrong while processing your boost. Please try again.",
+          icon: "error",
+        });
+      }
+    }
+  });
+};
+
+
+   
+ 
   return (
     <div>
       <button
         onClick={handleBoost}
         className="cursor-pointer flex items-center gap-1 px-3 py-2 rounded-md bg-green-500 text-white hover:bg-green-600 transition"
       >
-      <FaBolt />  Boost Priority
+        <FaBolt /> Boost Priority
       </button>
     </div>
   );
--- src/pages/AllIssues/AllIssues.jsx
+++ src/pages/AllIssues/AllIssues.jsx
@@ -6,46 +6,67 @@ import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import Card from "../../components/Shared/Card/Card";
 import Container from "../../components/Shared/Container";
+import LoadingSpinner from "../../components/Shared/LoadingSpinner";
+import useDebounce from "../../hooks/useDebounce";
+import Heading from "../../components/Shared/Heading";
 
 const categories = ["Road", "Electricity", "Water"];
 const statuses = ["Pending", "In Progress", "Resolved"];
 const priorities = ["High", "Normal"];
 
 const AllIssues = () => {
-  const [search, setSearch] = useState("");
   const [filterCategory, setFilterCategory] = useState("All");
   const [filterStatus, setFilterStatus] = useState("All");
   const [filterPriority, setFilterPriority] = useState("All");
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const limit = 8;
 
   const axiosSecure = useAxiosSecure();
 
-  const { data: allIssues = [] } = useQuery({
+  const {
+    data: allIssues = [],
+    isLoading,
+    isFetching,
+  } = useQuery({
     queryKey: [
       "allReportedIssues",
-      search,
+      debouncedSearch,
       filterCategory,
       filterStatus,
       filterPriority,
+      currentPage,
     ],
     queryFn: async () => {
       const res = await axiosSecure("/api/all-issues", {
         params: {
-          search,
+          search: debouncedSearch,
           category: filterCategory,
           status: filterStatus,
           priority: filterPriority,
+          page: currentPage,
+          limit,
         },
       });
       return res.data;
     },
+    keepPreviousData: true,
   });
+
+  const { issues = [], totalPages = 0 } = allIssues;
+
+  if (isLoading && !allIssues?.issues) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <Container>
-      <div className="p-6 md:p-10 bg-gray-50 min-h-screen">
+      <div className=" -mt-10 bg-gray-50 min-h-screen">
         <title>CivicFix | All Issues</title>
 
-        <h1 className="text-3xl font-bold text-primary mb-6">All Issues</h1>
+        <Heading title={"All Issues"} subtitle={"View and manage all reported civic issues in one place. Use search and filters to quickly find specific problems."} center={true}/>
 
         {/* Filters and Search */}
         <div className="flex flex-col md:flex-row md:items-center md:gap-4 mb-6">
@@ -57,7 +78,10 @@ const AllIssues = () => {
               placeholder="Search issues..."
               className="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
               value={search}
-              onChange={(e) => setSearch(e.target.value)}
+              onChange={(e) => {
+                setSearch(e.target.value);
+                setCurrentPage(1);
+              }}
             />
           </div>
 
@@ -105,20 +129,66 @@ const AllIssues = () => {
         </div>
 
         <div className="w-full text-center mt-10">
-          {!allIssues.length && (
+          {!issues.length && (
             <span className="font-semibold text-lg text-gray-400">
               No issues found
             </span>
           )}
         </div>
 
+        {isFetching && (
+          <div className="text-center text-sm text-gray-400 mb-4">
+            Loading...
+          </div>
+        )}
+
         {/* Issues Grid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          {allIssues.map((issue) => (
+          {issues.map((issue) => (
             <Card key={issue._id} issue={issue} />
           ))}
         </div>
       </div>
+
+      {totalPages > 1 && (
+        <div className="flex justify-center items-center gap-2 mt-10">
+          {/* Prev */}
+          <button
+            disabled={currentPage === 1}
+            onClick={() => setCurrentPage((p) => p - 1)}
+            className="px-3 py-1 border rounded disabled:opacity-40 cursor-pointer"
+          >
+            Prev
+          </button>
+
+          {/* Page Numbers */}
+          {[...Array(totalPages).keys()].map((num) => {
+            const page = num + 1;
+            return (
+              <button
+                key={page}
+                onClick={() => setCurrentPage(page)}
+                className={`cursor-pointer px-3 py-1 border rounded ${
+                  currentPage === page
+                    ? "bg-primary text-white"
+                    : "hover:bg-gray-100"
+                }`}
+              >
+                {page}
+              </button>
+            );
+          })}
+
+          {/* Next */}
+          <button
+            disabled={currentPage === totalPages}
+            onClick={() => setCurrentPage((p) => p + 1)}
+            className="px-3 py-1 border rounded disabled:opacity-40 cursor-pointer"
+          >
+            Next
+          </button>
+        </div>
+      )}
     </Container>
   );
 };
